a49bdc01bc649c0cc788ed5d258d21609a8298b3
ejerde <43681157+ejerde@users.noreply.github.com>
2018-10-31 12:40:49 -0700

    Add files via upload

---
+++ python_temp.py
@@ -0,0 +1,16 @@
+temperature = 25
+if temperature > 20:
+    print("whoa!")
+    print("nellie")
+elif temperature > 22:
+    print("not enough")
+print("fin")
+
+age = 22
+if age >= 18:
+    message ="Eligible"
+else:
+    message = "not eligible"
+    
+message = "Eligible" if age >= 18 else "not Eligible"
+print(message)
